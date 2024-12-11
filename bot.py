3840263ba0b8736184d7ded814cb929e746161f5
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-11 18:18:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,6 +84,7 @@ async def Jisshu_start():
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
+    PORT = "8080"
     await web.TCPSite(app, bind_address, PORT).start()
     await idle()
 

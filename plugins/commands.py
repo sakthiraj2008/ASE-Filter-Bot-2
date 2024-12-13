c4e409ae1375dfe8f92d50a2a48385992f1cc47b
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-13 07:56:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -139,9 +139,6 @@ async def start(client:Client, message):
                             InlineKeyboardButton('Rᴇғᴇʀ ⚜️', callback_data="reffff")
                         ]] 
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgQAAxkBAAEn9_ZmGp1uf1a38UrDhitnjOOqL1oG3gAC9hAAAlC74FPEm2DxqNeOmB4E") 
-        await asyncio.sleep(1)
-        await m.delete()
         await message.reply_photo(photo=random.choice(START_IMG), caption=script.START_TXT.format(message.from_user.mention, get_status(), message.from_user.id),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
